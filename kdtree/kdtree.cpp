2b0883d1d2b970c87c9709762c1e589b3082be69
Dustin <dustin.o.fox@gmail.com>
2018-04-19 16:26:35 -0700

    Added proper kd tree printing

--- kdtree/kdtree.cpp
+++ kdtree/kdtree.cpp
@@ -1,35 +1,39 @@
 #include "lib/mat.h"
 #include <iostream>
 
-void getData(Matrix &trainingData, Matrix &testData, char **labels);
-Matrix buildKDTree(const Matrix &data);
-bool buildKDTree_(Matrix &data, int currCol, int rowStart, int rowEnd);
+typedef char **cstring;
+
+void getData(Matrix &trainingData, Matrix &testData, cstring &labels);
+void buildKDTree(const Matrix &data, Matrix &tree);
+bool buildKDTree_R(Matrix &data, int currCol, int rowStart, int rowEnd);
 
 int main() {
-    char **labels;
+    cstring labels;
     Matrix trainingData;
     Matrix testData;
     Matrix KDTree;
     getData(trainingData, testData, labels);
-    KDTree = buildKDTree(trainingData);
-    KDTree.print();
+    buildKDTree(trainingData, KDTree);
+    KDTree.printLabeledRow(labels, "KDTree version of matrix");
 }
 
-void getData(Matrix &trainingData, Matrix &testData, char **labels) {
+void getData(Matrix &trainingData, Matrix &testData, cstring &labels) {
     // Read Data in from standard input
     labels = trainingData.readLabeledRow();
     testData.read();
 }
 
-Matrix buildKDTree(const Matrix &data) {
-    Matrix tree = Matrix(data);
-    if (!buildKDTree_(tree, 1, 0, tree.numRows() - 1)) {
-        std::cout << "\nERROR!!!\n";
+void buildKDTree(const Matrix &data, Matrix &tree) {
+    tree = Matrix(data);
+    if (!buildKDTree_R(tree, 1, 0, tree.numRows() - 1)) {
+        std::cerr << "ERROR while building kd tree.." << std::endl;
+        tree.setName("broken kd tree");
+    } else {
+        tree.setName("kd tree");
     }
-    return tree;
 }
 
-bool buildKDTree_(Matrix &data, int currCol, int startRow, int endRow) {
+bool buildKDTree_R(Matrix &data, int currCol, int startRow, int endRow) {
     // Feature wrap around
     if (currCol == data.numCols()) {
         currCol = 1;
@@ -42,12 +46,12 @@ bool buildKDTree_(Matrix &data, int currCol, int startRow, int endRow) {
         return true;
     } else {
         int medianRow = startRow + length / 2;
-        if (buildKDTree_(data , currCol + 1, startRow, medianRow - 1)) {
-            return buildKDTree_(data, currCol + 1, medianRow + 1, endRow);
+        if (buildKDTree_R(data , currCol + 1, startRow, medianRow - 1)) {
+            return buildKDTree_R(data, currCol + 1, medianRow + 1, endRow);
         } else {
             return false;
         }
     }
+    // Should never actually reach this point, but just to be sure.
+    return false;
 }
-
-void printKDTree()
